d015a142d58d213ef0621b025be87fa0273e2a25
JUNAIDHASAN75 <spiderss907@gmail.com>
2023-04-25 09:41:56 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCe0jQrbqaHJKYwybNvuKx74Alzc7aFOnk",
+  authDomain: "the-news-dragon-79507.firebaseapp.com",
+  projectId: "the-news-dragon-79507",
+  storageBucket: "the-news-dragon-79507.appspot.com",
+  messagingSenderId: "788643088912",
+  appId: "1:788643088912:web:b00f9d018b4e78d4505895"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './route/routes.jsx';
+// import router from './route/routes.js';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}>
+
+    </RouterProvider>
+  </React.StrictMode>,
+)
